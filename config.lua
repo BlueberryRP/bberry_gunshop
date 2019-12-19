2262c3821ad08b23b208a9fb5a4e9770b09abab8
BlueberryRP <58884050+BlueberryRP@users.noreply.github.com>
2019-12-19 06:16:52 +0500

    Fix coords

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config.NoMoney = 'Do not have enough money $'
 Config.Shoptext = 'Press (E) for action with gun shop'
 Config.LevelMissing = 'You do not have enough expirience' 
 Config.Coords = {
-	vector3(-322.26742553711,803.67279052734,117.88167572021),
+	vector3(-281.27, 780.74, 119.53),
 	vector3(-5508.1455078125,-2964.3273925781,-0.62878251075745),
 	vector3(2716.4248046875,-1285.4234619141,49.630435943604),
 	vector3(1322.6770019531,-1323.1640625,77.889030456543),
